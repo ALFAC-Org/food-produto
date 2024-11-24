d9d50f5ef7f7d796ac7e235cb7c36e33788a9b46
Leonardo Fraga <lafraga91@gmail.com>
2024-11-24 11:55:11 -0300

    Update log

--- src/main/java/br/com/alfac/foodproduto/infra/config/beans/DynamoDbConfiguration.java
+++ src/main/java/br/com/alfac/foodproduto/infra/config/beans/DynamoDbConfiguration.java
@@ -6,16 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.xspec.S;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "br.com.alfac.foodproduto.infra.persistence")
 public class DynamoDbConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(DynamoDbConfiguration.class);
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String awsDynamoEndpoint;
 
     @Value("${aws.accessKey}")
     private String accessKey;
@@ -23,20 +28,20 @@ public class DynamoDbConfiguration {
     @Value("${aws.secretKey}")
     private String secretKey;
 
-    @Value("${aws.dynamodb.endpoint}") // TODO: Talvez add o table nome aqui
-    private String awsDynamoEndpoint;
-
     @Value("${aws.sessionToken}")
     private String sessionToken;
-
-    // @Value("${aws.dynamodb.tablename}") 
-    // private String awsDynamoTableName;
 
     @Value("${aws.region}")
     private String awsRegion;
 
-     @Bean
+    @Bean
     public AmazonDynamoDB amazonDynamoDB() {
+        logger.info("AWS DynamoDB Endpoint: {}", awsDynamoEndpoint);
+        logger.info("AWS Access Key: {}", accessKey);
+        logger.info("AWS Secret Key: {}", secretKey);
+        logger.info("AWS Session Token: {}", sessionToken);
+        logger.info("AWS Region: {}", awsRegion);
+
         BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(
                 accessKey, secretKey, sessionToken);
 

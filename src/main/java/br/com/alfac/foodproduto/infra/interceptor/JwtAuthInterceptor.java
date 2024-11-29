8be8c5e3c0ba14cd24bb9071673fe29b46414edc
carlohcs <carlohcs@gmail.com>
2024-11-28 21:43:13 -0300

    feat: adiciona exceção de rota de health-check

--- src/main/java/br/com/alfac/foodproduto/infra/interceptor/JwtAuthInterceptor.java
+++ src/main/java/br/com/alfac/foodproduto/infra/interceptor/JwtAuthInterceptor.java
@@ -7,21 +7,36 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class JwtAuthInterceptor implements HandlerInterceptor {
 
     private static final String AUTEHNTICATED = "AUTHENTICATED";
     private static final String ANONYMOUS = "ANONYMOUS";
 
+    // List of routes to be ignored
+    private static final List<String> IGNORED_ROUTES = Arrays.asList(
+            "/api/v1/health-check"
+    );
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // Ignore the health-check route
+        String requestURI = request.getRequestURI();
 
-        String token = request.getHeader("auth");
-        
-        if(token == null){
+        // Ignore the routes in the list
+        if (IGNORED_ROUTES.contains(requestURI)) {
             return true;
         }
-        
+
+        String token = request.getHeader("auth");
+
+        if (token == null) {
+            return true;
+        }
+
         if (StringUtils.isBlank(token) || !token.startsWith("Bearer ")) {
             response.setStatus(401);
             return false;
